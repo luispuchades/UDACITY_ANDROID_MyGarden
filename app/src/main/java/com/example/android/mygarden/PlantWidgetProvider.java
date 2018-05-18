fe69d867636b510186a8dcd1cf5d36842ece9787
luispuchades <luispuchades@live.com>
2018-05-18 23:36:44 +0200

    Widget Service updated

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -27,51 +27,62 @@ import com.example.android.mygarden.ui.MainActivity;
 
 public class PlantWidgetProvider extends AppWidgetProvider {
 
+    // TODO (1) / COMPLETED: Modify updateAppWidget method to take an image recourse and call
+    // setImageViewResource to update the widget’s image
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+                                int imgRes, int appWidgetId) {
 
-        // Create an Intent to launch MainActivity when clicked 
+        // Create an Intent to launch MainActivity when clicked
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        // Construct the RemoteViews object 
+        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
-        // Widgets allow click handlers to only launch pending intents 
-        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+        // Update Image
+        views.setImageViewResource(R.id.widget_plant_image, imgRes);
 
-        // TODO (4) / COMPLETED: Create a PendingIntent for the PlantWateringService and
-        // setOnClickPendingIntent for widget_water_button.
-        // Add the watering service click handler
+        // Widgets allow click handlers to only launch pending intents
+        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+        // Add the wateringservice click handler
         Intent wateringIntent = new Intent(context, PlantWateringService.class);
         wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
-        PendingIntent wateringPendingIntent = PendingIntent.getService(context,0,wateringIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent wateringPendingIntent = PendingIntent.getService(context, 0, wateringIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
-
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them 
+        // TODO (2) / COMPLETED: Move the updateAppWidget loop to a new method called
+        // updatePlantWidgets and pass through the image recourse
+        // There may be multiple widgets active, so update all of them
+
+        // TODO (4) / COMPLETED: Call startActionUpdatePlantWidgets in onUpdate as well as in
+        // AddPlantActivity and PlantDetailActivity (add and delete plants)
+        PlantWateringService.startActionUpdatePlantWidgets(context);
+    }
+
+    public static void updatePlantWidgets(Context context, AppWidgetManager appWidgetManager, int
+            imgRes, int[] appWidgetIds) {
+
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context, appWidgetManager, imgRes, appWidgetId);
         }
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
-        // Perform any action when one or more AppWidget instances have been deleted 
+        // Perform any action when one or more AppWidget instances have been deleted
     }
 
     @Override
     public void onEnabled(Context context) {
-        // Perform any action when an AppWidget for this provider is instantiated 
+        // Perform any action when an AppWidget for this provider is instantiated
     }
 
     @Override
     public void onDisabled(Context context) {
-        // Perform any action when the last AppWidget instance for this provider is deleted 
+        // Perform any action when the last AppWidget instance for this provider is deleted
     }
 
-} 
+}
