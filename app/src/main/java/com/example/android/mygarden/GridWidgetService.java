f78981cb6c63a0e7ea6aa08e8f287974dffe6feb
luispuchades <luispuchades@live.com>
2018-05-20 11:22:35 +0200

    COMPLETED

--- app/src/main/java/com/example/android/mygarden/GridWidgetService.java
+++ app/src/main/java/com/example/android/mygarden/GridWidgetService.java
@@ -1,5 +1,21 @@
 package com.example.android.mygarden;
 
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -16,6 +32,7 @@ import com.example.android.mygarden.utils.PlantUtils;
 import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
 import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
 
+
 public class GridWidgetService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
@@ -30,6 +47,7 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     public GridRemoteViewsFactory(Context applicationContext) {
         mContext = applicationContext;
+
     }
 
     @Override
@@ -37,9 +55,7 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     }
 
-    /**
-     * Called on start when notifyAppWidgetViewDataChanged is called
-     */
+    //called on start and when notifyAppWidgetViewDataChanged is called
     @Override
     public void onDataSetChanged() {
         // Get all plant info ordered by creation time
@@ -56,7 +72,7 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     @Override
     public void onDestroy() {
-
+        mCursor.close();
     }
 
     @Override
@@ -65,15 +81,20 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
         return mCursor.getCount();
     }
 
+    /**
+     * This method acts like the onBindViewHolder method in an Adapter
+     *
+     * @param position The current position of the item in the GridView to be displayed
+     * @return The RemoteViews object to display for the provided postion
+     */
     @Override
     public RemoteViews getViewAt(int position) {
         if (mCursor == null || mCursor.getCount() == 0) return null;
         mCursor.moveToPosition(position);
         int idIndex = mCursor.getColumnIndex(PlantContract.PlantEntry._ID);
-        int plantTypeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
         int createTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_CREATION_TIME);
-        int waterTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry
-                .COLUMN_LAST_WATERED_TIME);
+        int waterTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+        int plantTypeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
 
         long plantId = mCursor.getLong(idIndex);
         int plantType = mCursor.getInt(plantTypeIndex);
@@ -81,20 +102,16 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
         long wateredAt = mCursor.getLong(waterTimeIndex);
         long timeNow = System.currentTimeMillis();
 
-
         RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.plant_widget);
 
         // Update the plant image
-        int imgRes = PlantUtils.getPlantImageRes(mContext, timeNow - createdAt, timeNow -
-                wateredAt, plantType);
+        int imgRes = PlantUtils.getPlantImageRes(mContext, timeNow - createdAt, timeNow - wateredAt, plantType);
         views.setImageViewResource(R.id.widget_plant_image, imgRes);
         views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
-
         // Always hide the water drop in GridView mode
         views.setViewVisibility(R.id.widget_water_button, View.GONE);
 
-        // Fill in the onClick PendingIntent Template using the specific plan Id for eacht item
-        // individually
+        // Fill in the onClick PendingIntent Template using the specific plant Id for each item individually
         Bundle extras = new Bundle();
         extras.putLong(PlantDetailActivity.EXTRA_PLANT_ID, plantId);
         Intent fillInIntent = new Intent();
@@ -102,6 +119,7 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
         views.setOnClickFillInIntent(R.id.widget_plant_image, fillInIntent);
 
         return views;
+
     }
 
     @Override
@@ -111,13 +129,12 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     @Override
     public int getViewTypeCount() {
-        // Treat all items in the GridView the same
-        return 1;
+        return 1; // Treat all items in the GridView the same
     }
 
     @Override
-    public long getItemId(int position) {
-        return position;
+    public long getItemId(int i) {
+        return i;
     }
 
     @Override
@@ -125,3 +142,4 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
         return true;
     }
 }
+
